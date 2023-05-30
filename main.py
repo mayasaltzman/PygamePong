7bfddb26061268c32ccc6eea1c6be9e4308dbed4
Maya Saltzman <mayasaltzman@Mayas-Air-5.hitronhub.home>
2023-05-30 15:26:39 -0400

    window is white

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame
 
 WIDTH,HEIGHT = 900,500
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+WIN = pygame.display.set_mode((WIDTH,HEIGHT)) #sets width and height of game window
+pygame.display.set_caption("Typing Test") #setting title of game window
+
+def draw_window():
+    WIN.fill((255,255,255))
+    pygame.display.update()
+
 
 def main():
     run = True
+    
+    #window runs until user exits window
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+        draw_window()
     pygame.quit()
 
 if __name__ == "__main__":
