8afa217792e6d884baebd9d7f61fccd86fb45324
Maya Saltzman <mayasaltzman@Mayas-Air-5.hitronhub.home>
2023-05-30 15:33:37 -0400

    text appearing

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 
+pygame.init()
+
 WIDTH,HEIGHT = 900,500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT)) #sets width and height of game window
 pygame.display.set_caption("Typing Test") #setting title of game window
 
+font = pygame.font.Font('freesansbold.ttf', 32)
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+text = font.render('Typing Test', True, green, blue)
+textRect = text.get_rect()
+
 def draw_window():
-    WIN.fill((255,255,255))
+    WIN.fill((255,255,255)) #fill window to be white
+    WIN.blit(text, textRect)
     pygame.display.update()
 
 
