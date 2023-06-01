b62c84b8bf11fc551dc631768f67a291a7c6adad
Maya Saltzman <mayasaltzman@Mayas-Air-5.hitronhub.home>
2023-06-01 13:58:50 -0400

    text is rendering centered

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 import os
 import random
-from textwrap import fill
+import textwrap
 
 pygame.init()
+
+FPS = 60 #frames per second
+clock = pygame.time.Clock()
 
 WIDTH,HEIGHT = 900,500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT)) #sets width and height of game window
@@ -17,6 +20,7 @@ black = (0, 0, 0)
 title = titleFont.render('Typing Test', True, black, white)
 titleRect = title.get_rect()
 titleRect.center = (WIDTH/2,30)
+
 
 def read_file():
     arr = os.listdir() #getting the list of files in the directory
@@ -36,18 +40,33 @@ def read_file():
 def draw_window(str):
     WIN.fill((white)) #fill window to be white
     WIN.blit(title, titleRect)
+
+    wrapped_text = textwrap.fill(str, 70)  # Adjust the desired line width as per your needs
+    
+    # Displaying the wrapped text
+    lines = wrapped_text.split('\n')
+    y = (HEIGHT // 2 - len(lines) * textFont.get_height() // 2) + 60 #centering the text along the y axis
+
+    for line in lines:
+        text = textFont.render(line, True, black)
+        textRect = text.get_rect()
+        textRect.center = (WIDTH/2, y)
+        WIN.blit(text, textRect)
+        y += textFont.get_height()
     
     #displaying the text read from the random file
-    text = textFont.render(fill(str,40), True, black, white)
-    textRect = text.get_rect()
-    textRect.center = (WIDTH/2, HEIGHT/2)
-    WIN.blit(text,textRect)
+    #text = textFont.render(str, True, black)
+    #textRect = text.get_rect()
+    #textRect.center = (WIDTH/2, HEIGHT/2)
+    #WIN.blit(text,textRect)
     
     pygame.display.update()
 
 
 def main():
+    
     run = True
+    clock.tick(FPS)
 
     #reading random file to avoid infinite file reads
     f = read_file()
